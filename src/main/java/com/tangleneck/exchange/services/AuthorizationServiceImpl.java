416715098c1e4c51185be848fe99a5bf8539bb03
Pedro Colomina <pedrocolomina@gmail.com>
2017-09-26 15:41:42 +0200

    Extract property as encrypted

--- src/main/java/com/tangleneck/exchange/services/AuthorizationServiceImpl.java
+++ src/main/java/com/tangleneck/exchange/services/AuthorizationServiceImpl.java
@@ -9,10 +9,10 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -26,13 +26,11 @@ public class AuthorizationServiceImpl implements AuthorizationService {
     @Autowired
     private CustomerRepository repository;
 
-    /*
-    @Autowired
-    private JavaMailSender emailSender;
-    */
-
     @Autowired
     private ReactiveMongoTemplate reactiveMongoTemplate;
+
+    @Value("${sendgrid.key}")
+    private String mySecretVar;
 
     @Override
     public Mono<Void> register(final RegistrationRequest registrationRequestMono) {
@@ -42,16 +40,10 @@ public class AuthorizationServiceImpl implements AuthorizationService {
 
     @Override
     public Mono<Void> confirmation(final String id) {
-        Query query = new Query(Criteria.where("_id").is(id));
-        Update update = Update
-                .update("confirmationDate", new Date());
-        return reactiveMongoTemplate.findAndModify(query, update, Customer.class).then();
-
-        /*
         return repository.findById(id)
                 .doOnNext(c -> c.setConfirmationDate(new Date()))
                 .doOnNext(repository::save).then();
-        */
+
     }
 
     @Override
@@ -94,14 +86,14 @@ public class AuthorizationServiceImpl implements AuthorizationService {
     }
 
     private Mono<Void> sendEmail(String email, String id) {
-        SendGrid sendgrid = new SendGrid("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");
+        SendGrid sendgrid = new SendGrid(mySecretVar);
         Email from = new Email("tangleneck@iota.com");
 
         String uri = "https://dashboard.heroku.com/apps/lit-ravine-74924/tangleneck/v1/confirmation?code=" + id;
         String subject = "Tangleneck - Email confirmation";
         Email to = new Email("pedrocolomina@gmail.com");
-        Content content = new Content("text/plain", "Hello, Thanks for registering to Tangleneck. \n"+
-                "To activate your account, click on the following link: \n "+ uri +")");
+        Content content = new Content("text/plain", "Hello, Thanks for registering to Tangleneck. \n" +
+                "To activate your account, click on the following link: \n " + uri + ")");
         Mail mail = new Mail(from, subject, to, content);
         Request request = new Request();
         request.setMethod(Method.POST);
@@ -112,16 +104,6 @@ public class AuthorizationServiceImpl implements AuthorizationService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        /*
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("pepito@gmail.com");
-        message.setTo(email);
-        message.setSubject("Tangleneck - Email confirmation");
-        message.setText("Hello, Thanks for registering to Tangleneck. \n" +
-                "To activate your account, click on the following link: \n" + uri);
-        emailSender.send(message);
-       */
         return Mono.empty();
     }
 }
